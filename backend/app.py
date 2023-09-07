740c5960a81a2a0938c49a29b84d0f4a83a97c5f
lanroo <yladacs@gmail.com>
2023-09-07 01:36:00 -0300

    implementação do backend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify, render_template
-import youtube_dl
+import yt_dlp
 
 app = Flask(__name__)
+
+# Verifique se a versão do yt_dlp é igual ou superior a 2021.12.17
+yt_version = yt_dlp.version.__version__
+if yt_version < "2021.12.17":
+    raise Exception("Por favor, atualize yt-dlp para a versão 2021.12.17 ou superior.")
 
 # Defina uma rota raiz para renderizar o template HTML do frontend
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Defina a rota '/download' para processar a solicitação de download
+# Rota '/download' para processar a solicitação de download
 @app.route('/download', methods=['POST'])
 def download_video():
     try:
@@ -20,7 +25,7 @@ def download_video():
             'outtmpl': 'downloads/%(title)s.%(ext)s',
         }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
             video_title = info['title']
 
@@ -29,7 +34,6 @@ def download_video():
         return jsonify({'message': f'Download do vídeo "{video_title}" concluído com sucesso'})
     except Exception as e:
         return jsonify({'error': str(e)})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/static/app.js
+++ backend/static/app.js
@@ -3,7 +3,8 @@ const baixarVideoButton = document.getElementById("baixar-video");
 const baixarAudioButton = document.getElementById("baixar-audio");
 const formatContainer = document.getElementById("format-container");
 const formatSelect = document.getElementById("format");
-const downloadButton = document.querySelector("button[type=submit]");
+const downloadButton = document.getElementById("submit-button");
+const downloadLink = document.getElementById("download-link");
 
 // Adiciona um evento de clique para o botão "Baixar Vídeo"
 baixarVideoButton.addEventListener("click", function() {
@@ -13,6 +14,8 @@ baixarVideoButton.addEventListener("click", function() {
         <option value="mp4">MP4</option>
         <option value="mkv">MKV</option>
     `;
+    // Exibe o botão de download
+    downloadButton.style.display = "block";
 });
 
 // Adiciona um evento de clique para o botão "Baixar Áudio"
@@ -23,14 +26,43 @@ baixarAudioButton.addEventListener("click", function() {
         <option value="mp3">MP3</option>
         <option value="wav">WAV</option>
     `;
+    // Exibe o botão de download
+    downloadButton.style.display = "block";
 });
 
-// Adiciona um evento de clique para o botão "Download"
+// Adiciona um evento de clique para o botão de download
 downloadButton.addEventListener("click", function() {
     // Obtém o formato selecionado
     const selectedFormat = formatSelect.value;
 
-    // Simula o download do arquivo com base no formato selecionado
-   
-    alert(`Baixando arquivo no formato ${selectedFormat}`);
+    // Obtém a URL do vídeo a partir do campo de entrada
+    const videoUrl = document.getElementById("video-url").value;
+
+    // Cria um objeto de dados para enviar no corpo da solicitação POST
+    const requestData = {
+        "video-url": videoUrl,
+        "format": selectedFormat
+    };
+
+    // Realiza uma solicitação POST para a rota /download no servidor Flask
+    fetch("/download", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Lida com a resposta do servidor aqui
+        if (data.message !== undefined) {
+            // Se a resposta contém uma mensagem, exibe um link de download
+            downloadLink.innerHTML = `<a href="${data.message}" download>Clique aqui para baixar</a>`;
+        } else {
+            alert("Mensagem não definida no servidor.");
+        }
+    })
+    .catch(error => {
+        console.error("Erro ao fazer a solicitação ao servidor:", error);
+    });
 });
